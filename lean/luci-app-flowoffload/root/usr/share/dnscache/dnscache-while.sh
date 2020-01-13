a945126040611b9b972aa1eb3ff6f68c7cc6c21a
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2020-01-13 11:03:23 +0800

    upgrade

--- lean/luci-app-flowoffload/root/usr/share/dnscache/dnscache-while.sh
+++ lean/luci-app-flowoffload/root/usr/share/dnscache/dnscache-while.sh
@@ -2,6 +2,7 @@
 
 sleeptime=60
 logfile="/var/log/dnscache.file"
+adglogfile="/etc/AdGuardHome/data/querylog.json"
 dns_enable=$(uci get flowoffload.@flow[0].dns 2>/dev/null)
 dnscache_enable=$(uci get flowoffload.@flow[0].dnscache_enable 2>/dev/null)
 
@@ -12,26 +13,30 @@ if [ $logrow -ge 500 ];then
     echo "$curtime Log条数超限，清空处理！" >> ${logfile}
 fi
 
+adglogrow=$(grep -c "" ${adglogfile})
+if [ $adglogrow -ge 5000 ];then
+    cat /dev/null > ${adglogfile}
+    echo "$curtime adgLog条数超限，清空处理！" >> ${logfile}
+fi
+
 }
 
 while [ $dns_enable -eq 1 ];
 do
 curtime=`date "+%H:%M:%S"`
 echo "$curtime online! "
+clean_log
 if [ $dns_enable -eq 1 ]; then
 	if [ $dnscache_enable = "3" ];  then
 		if ! pidof AdGuardHome>/dev/null; then
-			AdGuardHome -c /etc/AdGuardHome/AdGuardHome.yaml -w /etc/AdGuardHome >/dev/null 2>&1 &
+			AdGuardHome -c /var/etc/AdGuardHome/AdGuardHome.yaml -w /var/etc/AdGuardHome -h 0.0.0.0 -p 3333 >/dev/null 2>&1 &
 			echo "$curtime 重启服务！" >> ${logfile}
-		fi
-		if ! pidof dnscache>/dev/null; then
-			dnscache -f /var/run/dnscache/dnscache.conf -d
 		fi
 	else
 		if ! pidof dnscache>/dev/null; then
 			if [ $dnscache_enable = "1" ];  then
 			/usr/sbin/dnscache -c /var/etc/dnscache.conf -d
-			elif [ $dnscache_enable = "2" ] || [ $dnscache_enable = "3" ];  then
+			elif [ $dnscache_enable = "2" ];  then
 			dnscache -f /var/run/dnscache/dnscache.conf -d
 			fi
 			echo "$curtime 重启服务！" >> ${logfile}
